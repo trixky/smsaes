226ed8f538280566b93eecff868c24a5230a5fb2
Mathis Bois <mathis.bois@outlook.fr>
2022-09-03 10:23:24 +0200

    [Dev] home: start the home page

---
+++ app/pages/contact.svelte
@@ -0,0 +1,31 @@
+<script>
+  let message = "Blank Svelte Native App";
+  export let contact;
+</script>
+
+<page>
+  <actionBar title="Contact Detail" />
+  <gridLayout>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label
+      class="info"
+      horizontalAlignment="center"
+      verticalAlignment="middle"
+      textWrap="true"
+    >
+      <formattedString>
+        <span class="fas" text="&#xf135;" />
+        <span text=" {contact.name + ' ' + contact.phone_number}" />
+      </formattedString>
+    </label>
+  </gridLayout>
+</page>
+
+<style>
+  .info .fas {
+    color: #3a53ff;
+  }
+  .info {
+    font-size: 20;
+  }
+</style>
--- app/pages/home.svelte
+++ app/pages/home.svelte
@@ -1,30 +1,51 @@
-<script lang="typescript">
-  let message: string = "Blank Svelte Native App";
+<script>
+  import Contact from "./contact.svelte";
+  import { navigate } from "svelte-native";
+
+  let contacts = [
+    {
+      name: "trixky",
+      phone_number: "+33 000000000",
+    },
+    {
+      name: "chat",
+      phone_number: "+33 090909090",
+    },
+  ];
 </script>
 
 <page>
   <actionBar title="Svelte Native App" />
-  <gridLayout>
-    <!-- svelte-ignore a11y-label-has-associated-control -->
-    <label
-      class="info"
-      horizontalAlignment="center"
-      verticalAlignment="middle"
-      textWrap="true"
-    >
-      <formattedString>
-        <span class="fas" text="&#xf135;" />
-        <span text=" {message}" />
-      </formattedString>
-    </label>
-  </gridLayout>
+  <scrollView orientation="vertical">
+    <stackLayout>
+      {#each contacts as contact}
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <button
+          class="contact-button"
+          on:tap={() =>
+            navigate({
+              page: Contact,
+              props: { contact },
+            })}
+        >
+          <formattedString>
+            <span text={contact.name} class="contact-name" />
+            <span text={"  " + contact.phone_number} />
+          </formattedString>
+        </button>
+      {/each}
+    </stackLayout>
+  </scrollView>
 </page>
 
 <style>
-  .info .fas {
-    color: #3a53ff;
+  .contact-button {
+    text-align: left;
+    border-width: 2 0 0 0;
+    border-color: rgb(99, 224, 99);
   }
-  .info {
-    font-size: 20;
+  .contact-name {
+    text-decoration: underline;
+    padding-right: 30px;
   }
 </style>
