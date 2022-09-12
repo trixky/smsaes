948cf768d53c3654fc2c65dadc55b9dfb3b34551
Mathis Bois <mathis.bois@outlook.fr>
2022-09-12 13:08:50 +0200

    [Fix] firstname: improve the check of the firstname input

--- app/utils/input_checker.ts
+++ app/utils/input_checker.ts
@@ -6,8 +6,8 @@ export function checkPhoneNumber(phone_number: string): Error | null {
 }
 
 export function checkFirstname(phone_number: string): Error | null {
-  if (phone_number.length < 2) {
-    return new Error("Firstname is too short");
+  if (phone_number.length === 0) {
+    return new Error("Firstname is missing");
   }
   return null;
 }
